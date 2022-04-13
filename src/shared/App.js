24e8aadc0165eb38785e85db3200d1ddbaed1a3f
devkevinsoon <parkts7777@gmail.com>
2022-04-14 01:51:03 +0900

    [박태순]getpost 수정_20220414

--- src/shared/App.js
+++ src/shared/App.js
@@ -2,26 +2,24 @@ import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
-import { TiPlus } from "react-icons/ti";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Header } from "../components/index";
 import { Grid, Button } from "../elements";
 import { Login, Signup, Main, Detail, PostWrite } from '../pages/index';
 import { actionCreators as userActions } from "../redux/modules/user";
 import Test from "../pages/Test";
 
+
 function App() {
     const dispatch = useDispatch();
-    const is_login = useSelector(state => state.user.is_login);
-    const token = localStorage.getItem("token");
+    const is_local = localStorage.getItem("token") ? true : false;
     useEffect(() => {
-        if(!token){
-            console.log("없어")
-            return;
-        } else {
+        if(is_local){
             dispatch(userActions.loginCheckApi());
         };
     },[]);
+    
+    
     return (
         <React.Fragment>
             <Grid>
@@ -39,5 +37,4 @@ function App() {
         </React.Fragment>
     );
 }
-
 export default App;
